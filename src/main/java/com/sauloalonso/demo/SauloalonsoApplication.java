4e638e6f30aae09879deccdf1b55d08e23a11699
Saulo Alonso de Souza Pereira <salonsos@indracampos.local>
2021-12-14 21:25:36 -0300

    Abublé!

---
+++ src/main/java/com/sauloalonso/demo/SauloalonsoApplication.java
@@ -0,0 +1,14 @@
+package com.sauloalonso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauloalonsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauloalonsoApplication.class, args);
+		System.out.println("hello!");
+	}
+
+}
---
+++ src/test/java/com/sauloalonso/demo/SauloalonsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauloalonso.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauloalonsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
